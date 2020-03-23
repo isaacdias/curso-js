2e268aa23dc58da862aa5d3d462a3c1cea864dda
Isaac Dias <isaacardias@gmail.com>
2020-03-23 14:09:29 -0300

    aula sobre console.log

---
+++ console.log/index.js
@@ -0,0 +1,8 @@
+console.log('Isaac Dias'); // string = texto
+console.log("Isaac 'Dias'");
+console.log(`Isaac Dias`);
+
+console.log(123456); // number = numero 
+console.log(1.5); // number = numero
+
+console.log(35, 15.85, 'Isaac Dias');
