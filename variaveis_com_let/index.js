91cba074297158e2b7aaca9c68974b31b6c160f7
Isaac Dias <isaacardias@gmail.com>
2020-03-23 15:21:53 -0300

    Como declarar variaveis usando let

---
+++ variaveis_com_let/index.js
@@ -0,0 +1,25 @@
+let nome = 'Isaac'; // utilizar 'let' para declarar variaveis
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');
+
+
+let nomeCliente; // como declarar uma variavel sem incializar 
+nomeCliente = 'Rafael'; // inicializando a variavel
+
+console.log(nomeCliente);
+
+
+// Não podemos criar variaveis com palavras reservadas
+// Variaveis precisam ter nomes significativos 
+// não pode começar o nome da variavel com numero
+// Não pode conter espacos ou tracos
+// Utilizamos camelCase
+// Case-sensitive
+// Nao podemos redeclarar variaveis  com let
+
+
+
