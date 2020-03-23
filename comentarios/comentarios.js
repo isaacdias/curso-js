2bdb78d6d06da35a2130cb0524052d7226c20da1
Isaac Dias <isaacardias@gmail.com>
2020-03-23 14:10:56 -0300

    aula sobre comentarios no javascript

---
+++ comentarios/comentarios.js
@@ -0,0 +1,13 @@
+// no JS usamos duas barras para comentarios de linha
+console.log('Olá mundo!'); // outro comentario
+console.log('linha 1'); // mais um comentario
+console.log('linha 2'); 
+// outro comentario
+
+// comentario de bloco de codigo
+
+/* 
+este é um comentario 
+de bloco de codigo
+*/
+
