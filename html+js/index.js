f005625b6b9ecfde06554b25f19cf2cc93055ccb
Isaac Dias <isaacardias@gmail.com>
2020-03-23 14:12:07 -0300

    exemplo de implementação do js no html

---
+++ html+js/index.js
@@ -0,0 +1,6 @@
+// será exibido no console do navegador 
+console.log('Olá mundo!');
+console.log('Este trecho será exibido no console do navegador');
+alert('Olá mundo!');
+
+// Lembrar de adicionar o caminho dentro da tag de abertura do script
