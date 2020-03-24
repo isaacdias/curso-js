2bd0411304e5f352067ae8acee7e2832f1ae54ac
Isaac Dias <isaacardias@gmail.com>
2020-03-24 17:47:10 -0300

    tipos de dados primitivos

---
+++ tipos_de_dados_primitivos/index.js
@@ -0,0 +1,9 @@
+const nome = 'Luiz'; //string
+const nome1 = "Luia"; //string
+const nome2 = `Luiz`; //string
+const num1 = 10; // number
+const num2 = 10.52; //number
+let nomeAluno; // undefined -> não apona para local nenhum da memoria
+let sobrenomeAluno = null; // null -> não aponta para local nenhum da memoria
+const aprivado = true; // boolean -> true or false (lógico)
+
