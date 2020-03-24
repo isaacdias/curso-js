be6304a707183347fb6bdd7741ca9c1ffd828899
Isaac Dias <isaacardias@gmail.com>
2020-03-24 18:38:03 -0300

    aula sobre operadores aritméticos no JS

---
+++ operadores_aritmeticos/index.js
@@ -0,0 +1,56 @@
+/** Operadores Aritméticos
+ 
+ + Adição / Concatenação
+ */
+// exemplo adição
+const num1 = 2;
+const num2 = 10;
+console.log(num1 + num2);
+
+// exemplo concatenação
+const num3 = '2'; //string
+const num4 = 10;
+console.log(num3 + num4);
+
+/**
+ - Subtração
+ */
+console.log(num1 - num2);
+
+/**
+ / Divisão
+ */
+console.log(num1 / num2);
+
+/**
+ * Multiplicação
+ */
+console.log(num1 * num2);
+
+/**
+ ** Potenciação
+ */
+console.log(num1 ** num2);
+
+/**
+ % Resto da divisão
+ */
+console.log(num1 % num2);
+
+/**
+ ++ incremento
+ */
+let contador = 1;
+contador++;
+contador++;
+++contador; // o operador pode ser colocado antes 
+console.log(contador);
+
+/**
+ -- decremento
+ */
+let contador2 = 10;
+contador2--;
+contador2--;
+console.log(contador2);
+
