9349fdd35816ba8885d7ffaa02af3585ecc3339c
Isaac Dias <isaacardias@gmail.com>
2020-03-25 19:45:59 -0300

    exercicio com strings

---
+++ strings /exercicio/js/script.js
@@ -0,0 +1,10 @@
+const nome = prompt('Digite seu nome completo:');
+document.body.innerHTML = `O seu nome é: <strong>${nome}</strong> <br />`;
+document.body.innerHTML += `O seu nome tem <strong>${nome.length}</strong> caracteres <br />`;
+document.body.innerHTML += `A segunda letra do nome é: <strong>${nome.charAt(1)}</strong> <br />`;
+document.body.innerHTML += `Qual o primeiro indice da letra A no seu nome? <strong>${nome.indexOf('a')}</strong> <br />`;
+document.body.innerHTML += `Qual o ultimo indice da letra no seu nome? <strong>${nome.lastIndexOf('a')}</strong> <br />`;
+document.body.innerHTML += `As ultimas três letras do seu nome são: <strong>${nome.slice(-3)}</strong> <br />`;
+document.body.innerHTML += `As palavras do seu nome são: <strong>${nome.split(' ')}</strong> <br />`;
+document.body.innerHTML += `Seu nome com letras maiúsculas: <strong>${nome.toUpperCase()}</strong> <br />`;
+document.body.innerHTML += `Seu nome com letras minúsculas: <strong>${nome.toLowerCase()}</strong> <br />`;
