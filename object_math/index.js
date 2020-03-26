baea831337a8686b2a1d07cf12ef5e25818a0409
Isaac Dias <isaacardias@gmail.com>
2020-03-26 14:27:48 -0300

    Alguns metodos da object Math

---
+++ object_math/index.js
@@ -0,0 +1,23 @@
+let num1 = 9.54578;
+
+// Usarei o console.log para exemplificar os metodos do obectj Math
+
+console.log(Math.floor(num1)); // arrendoda para baixo = # 9
+
+console.log(Math.ceil(num1)); // arrendoda para baixo = # 10
+
+console.log(Math.round(num1)); // arrendoda para o número mais proximo
+
+console.log(Math.max(1, 2, 3, 5, 100, 1500, -6, -50 -300, 2056)); // retorna o maior numero de uma lista
+
+console.log(Math.min(1, 2, 3, 5, 100, 1500, -6, -50 -300, 2056)); // retorna o menor numero de uma lista
+
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5); // random de um numero entre 5 e 10 
+console.log(aleatorio);
+
+console.log(Math.PI); // retorna o valor de PI
+
+console.log(Math.pow(2, 10)); // retorna a potenciação do primeiro argumento elevado ao segundo argumento
+
+
+
