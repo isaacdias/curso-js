c8468d3cb08285bd56007b42f920f90ea81e056a
Isaac Dias <isaacardias@gmail.com>
2020-03-27 17:02:17 -0300

    exercicio com funções, arrays e objetos no JS

---
+++ teste_functions_array_object/js/script.js
@@ -0,0 +1,29 @@
+function meuEscopo() {
+    const form = document.querySelector('.formulario'); // aqui selecionamos o formulario dentro de document
+    const resultado = document.querySelector('.resultado');
+
+    const pessoas = []; // array que receberá os objetos
+
+    function recebeEventoForm(evento) {
+        evento.preventDefault(); // aqui definimos para não acontecer o evento padrão que deveria acontecer
+
+        const nome = form.querySelector('.nome'); // aqui eu pego o elemento dentro da variavel form que criei
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+
+        pessoas.push({ // objeto que será enviado para o array
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
+
+        console.log(pessoas);
+
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`; // mostrando o resultao no html
+    }
+
+    form.addEventListener('submit', recebeEventoForm);
+}
+meuEscopo();
