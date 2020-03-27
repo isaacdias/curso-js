8f2b08b2a4523d2c1338f97b2d0c42501c5b1dc0
Isaac Dias <isaacardias@gmail.com>
2020-03-27 15:11:15 -0300

    adicionando comentarios sobre os metodos do obejto

--- objects/index.js
+++ objects/index.js
@@ -4,19 +4,20 @@ const pessoa1 = {     // usamos as chaves para criar um objeto
     sobrenome: 'Dias',
     idade: 32,
 
-// Criando um metodo dentro do objeto
+// Criando um metodo que informa a idade atual dentro do objeto
     fala() {
         console.log(`A minha idade atual é ${this.idade} anos.`);
     }, // metodos precisam ser separados por virgula
 
+// metodo que incrementa a idade do obejto
     incrementaIdade() {
         this.idade++
     }
 };
 
-pessoa1.fala();
-pessoa1.incrementaIdade();
-pessoa1.fala();
+pessoa1.fala(); // chamando o metodo fala
+pessoa1.incrementaIdade(); // chamando o metodo para incrememtar idade
+pessoa1.fala(); // novamente chamando o metodo fala
 
 console.log(pessoa1.nome); // acessando um atributo do objeto (nome)
 console.log(pessoa1.sobrenome); // acessando um atributo do objeto (sobrenome)
