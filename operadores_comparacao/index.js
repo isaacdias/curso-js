a7e6a2914145b5cee88127d7b428d69642388192
Isaac Dias <isaacardias@gmail.com>
2020-03-27 20:30:02 -0300

    estudando operadores de comparação

---
+++ operadores_comparacao/index.js
@@ -0,0 +1,25 @@
+/*
+Operadores de comparação
+
+> maior que
+>= maior que ou igual a
+< menor que
+<+ menor que ou igual a
+=== igualdade estrita (valor e tipo)
+!== diferente estrito (valor e tipo)
+
+*/
+
+console.log(10 > 5);
+console.log(10 >= 10);
+console.log(10 < 5);
+console.log(10 <= 10);
+console.log(10 == 10);
+console.log(10 === 10);
+console.log(10 != 10);
+console.log(10 !== 10);
+
+
+
+
+
