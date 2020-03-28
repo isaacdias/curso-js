d76c70e46dc6da354d8f570580bf22b1901c54c2
Isaac Dias <isaacardias@gmail.com>
2020-03-27 21:30:00 -0300

    entendendo os valores FALSY que retornam falso

---
+++ 02-logica/short_circuit/index.js
@@ -0,0 +1,22 @@
+/*
+Exemplos de FALSY
+false
+0
+' ' " " ` `
+null
+undefined
+NaN
+*/
+
+function falaOi() {
+    return 'Oi';
+}
+
+const vaiExecutar = 0;
+console.log(vaiExecutar && falaOi()); // condicional de corma simples
+
+console.log(0 || false || null || 'Isaac' || true);
+
+const corUsuario = null;
+const corPadrao = corUsuario || 'preto'
+console.log(corPadrao);
