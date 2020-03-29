e257e36c932214ebedfb91bf319777bf11be0043
Isaac Dias <isaacardias@gmail.com>
2020-03-29 14:35:47 -0300

    alteração no comentário

--- 02-logica/operacao_ternaria/index.js
+++ 02-logica/operacao_ternaria/index.js
@@ -2,3 +2,6 @@ const pontuacaoUsuario = 1000;
 const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP': 'Usuário comum';
 
 console.log(nivelUsuario);
+
+
+// condição ? 'valor para verdadeiro' : 'Valor para falso';
