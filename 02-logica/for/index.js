3f9478bfd3eb77f9d8075aceaa55d1d604175bf4
Isaac Dias <isaacardias@gmail.com>
2020-03-31 16:09:38 -0300

    verificando se é par com for

--- 02-logica/for/index.js
+++ 02-logica/for/index.js
@@ -6,4 +6,11 @@ for (variavel de controle; condição; incremento ou decremento){
 
 for (let i = 0; i <= 5; i++) {
     console.log(i);
-} 
+}
+
+
+// verificando se é par
+for (let i2 = 0; i2 <= 5; i2++) {
+    const par = i2 % 2 === 0? 'Par': 'Ímpar';
+    console.log(i2, par);
+}
