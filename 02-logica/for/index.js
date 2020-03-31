0d7892dab7b5f04a2a4d9ab1b70f89d8e7ac6a3f
Isaac Dias <isaacardias@gmail.com>
2020-03-31 16:19:43 -0300

    percorrendo o array com for

--- 02-logica/for/index.js
+++ 02-logica/for/index.js
@@ -14,3 +14,10 @@ for (let i2 = 0; i2 <= 5; i2++) {
     const par = i2 % 2 === 0? 'Par': 'Ímpar';
     console.log(i2, par);
 }
+
+
+// percorrendo o array com o laço for
+const frutas = ['Maçã', 'Pêra', 'Uva'];
+for (let i3 = 0; i3 < frutas.length; i3++) {
+    console.log(frutas[i3]);
+}
