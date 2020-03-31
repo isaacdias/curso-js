127d18b598647a4f65c6f6f7b30c166130b2d87e
Isaac Dias <isaacardias@gmail.com>
2020-03-31 00:24:46 -0300

    exemplo com array contendo outros arrays

--- 02-logica/atribuicao_via_desestruturacao_arrays/index.js
+++ 02-logica/atribuicao_via_desestruturacao_arrays/index.js
@@ -38,3 +38,7 @@ const numeros3 = [1000, 2000, 3000, 4000, 5000, 6000,];
 let [primeiroNumero3, , terceiroNumero3, , quintoNumero3] = numeros2; // aqui atribui os valores do array em variaveis separadas deuma vez só.
 console.log(primeiroNumero3, terceiroNumero3, quintoNumero3);
 
+// Acessando indics de uma array dentro de outro array
+
+numeros4 = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]; // um array com outros arrays dentro dele
+console.log(numeros4[2][0]); // acessando o numero 7 
