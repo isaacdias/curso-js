808aba9815a50f5409cb1d68d3e3cb3371316812
Isaac Dias <isaacardias@gmail.com>
2020-03-30 23:49:28 -0300

    add comentário sobre o que está acontecendo no código

--- 02-logica/atribuicao_via_desestruturacao_arrays/index.js
+++ 02-logica/atribuicao_via_desestruturacao_arrays/index.js
@@ -6,7 +6,14 @@ let c = 'C';
 o que está do lado esquerdo é a desestruturação das variaveis 
 sedo reatribuidas com o valor array ao lado.
 */
-const numeros = [1, 2, 3];
+const letras = [b, c ,a];
 [a, b, c] = numeros; 
+
+/*
+Após o codigo acima as variaveis receberão os valores de acordo com os valores anteriores.
+a = valor de b
+b = valor de c
+c recebe valor de a
+*/
 
 console.log(a, b , c);
