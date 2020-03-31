db8530cccd497effce1fd451c5e28aba0c8597ee
Isaac Dias <isaacardias@gmail.com>
2020-03-30 23:46:29 -0300

    inserindo comentário

--- 02-logica/atribuicao_via_desestruturacao_arrays/index.js
+++ 02-logica/atribuicao_via_desestruturacao_arrays/index.js
@@ -2,7 +2,11 @@ let a = 'A';
 let b = 'B';
 let c = 'C';
 
+/*
+o que está do lado esquerdo é a desestruturação das variaveis 
+sedo reatribuidas com o valor array ao lado.
+*/
 const numeros = [1, 2, 3];
-[a, b, c] = numeros;
+[a, b, c] = numeros; 
 
 console.log(a, b , c);
