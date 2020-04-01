7dcd330f0024570c120cc361dee95913a6739789
Isaac Dias <isaacardias@gmail.com>
2020-04-01 15:37:40 -0300

    exepmlo de for in com array e objeto

---
+++ 02-logica/for_in/index.js
@@ -0,0 +1,19 @@
+// Ffor in com arrays 
+
+const frutas = ['Pêra', 'Maçã', 'Uva'];
+
+for (let indice in frutas) {
+    console.log(frutas[indice]);
+};
+
+// for in utilizado em objetos
+
+const pessoa = {
+    nome: 'Isaac',
+    sobrenome: 'Dias',
+    Idade: 32,
+};
+
+for (let chave  in pessoa) {
+    console.log(chave, pessoa[chave]);
+};
