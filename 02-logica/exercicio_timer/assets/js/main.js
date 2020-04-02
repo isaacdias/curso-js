1516cda92c69465af75c8fec7c64fb9524fdce2c
Isaac Dias <isaacardias@gmail.com>
2020-04-02 02:16:58 -0300

    desenvolvimento de uma página com um timer

---
+++ 02-logica/exercicio_timer/assets/js/main.js
@@ -0,0 +1,63 @@
+function getTimeForSeconds (seconds) {
+    const date = new Date( seconds * 1000);
+    return date.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'GMT',        
+    });
+}
+
+const clock = document.querySelector('.clock');
+const start = document.querySelector('.start');
+const pause = document.querySelector('.pause');
+const reset = document.querySelector('.reset');
+let seconds = 0;
+let timer;
+
+function startTimer() {
+    timer = setInterval (function(){
+        seconds++;
+        clock.innerHTML = getTimeForSeconds(seconds);
+    }, 1000);
+}
+
+start.addEventListener('click', function(event) {
+    clock.classList.remove('paused')
+    clearInterval(timer);
+    startTimer();
+});
+pause.addEventListener('click', function(event) {
+    clearInterval(timer);
+    clock.classList.add('paused')
+});
+reset.addEventListener('click', function(event) {
+    clearInterval(timer);
+    clock.classList.remove('paused');
+    clock.innerHTML = '00:00:00'
+    seconds = 0;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
