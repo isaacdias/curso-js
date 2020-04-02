7c60516ada2076e21f95975c364403ac4f2db022
Isaac Dias <isaacardias@gmail.com>
2020-04-02 00:30:08 -0300

    configurando hora, minutos e segundos para dois digitos

--- 02-logica/setinterval_settimeout/index.js
+++ 02-logica/setinterval_settimeout/index.js
@@ -2,6 +2,9 @@ function mostraHora(){
     let data = new Date;
 
     return data.toLocaleTimeString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
         hour12: false
 } );
 }
