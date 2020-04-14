ea03a6f3d70ee5bd98da45ff41fc354a2c5ac8e5
Isaac Dias <isaacardias@gmail.com>
2020-04-13 21:38:16 -0300

    estudando funções callback

---
+++ 03-funcoes/funcoes_callback/index.js
@@ -0,0 +1,54 @@
+// Funções de callback
+// funções que acontecem quando uma ação é finalizada
+
+function rand(min = 1000, max = 3000) {
+ 	const num = Math.random() * (max - min) + min;
+ 	return Math.floor(num);
+}
+
+function f1(callback) {
+	setTimeout(function() { // setTimeout criado apenas para simular algo que demoraria para acontecer
+		console.log('f1');
+		if (callback) callback(); 
+	}, rand());
+}
+
+function f2(callback) {
+	setTimeout(function() {
+		console.log('f2');
+		if (callback) callback(); 
+	}, rand());
+}
+
+function f3(callback) {
+	setTimeout(function() {
+		console.log('f3');
+		if (callback) callback(); 
+	}, rand());
+}
+
+f1(f1callback);
+
+function f1callback() {
+	f2(f2callback);
+}
+
+function f2callback() {
+	f3(f3callback);
+}
+
+function f3callback() {
+	console.log('Olá mundo!');
+}
+
+
+
+/*
+f1(function() {
+	f2(function() {
+		f3(function() {
+			console.log('Olá mundo!');
+		});
+	});
+});
+*/
