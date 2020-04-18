301206ab2a5054d0165bd9824aeb8cdb6a2e2e4d
isaacdias <isaacrdias@gmail.com>
2020-04-18 14:54:46 -0300

    alterando o comentario

--- 04-arrays/concatenando_arrays/index.js
+++ 04-arrays/concatenando_arrays/index.js
@@ -6,7 +6,7 @@ console.log(a3);
 const a4 = ai.concat(a2, [7, 8, 9], 'Luiz'); // utilizando o concat
 console.log(a4); 
 
-const a5 = [...a1, ...a2] // utilizando o spread operator
+const a5 = [...a1, ...a2] // utilizando o spread operator com arrays
 console.log(a5);
 
 const a6 = [...a1, ...a2, ...[7, 8, 9], 'Luiz'] // utilizando o spread  
