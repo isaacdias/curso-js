f705be5746be1fea0d673e689b4a24a5dc636250
isaacdias <isaacrdias@gmail.com>
2020-04-18 14:24:28 -0300

    Como concatenar arrays

---
+++ 04-arrays/concatenando_arrays/index.js
@@ -0,0 +1,10 @@
+const a1 = [1, 2, 3];
+const a2 = [4, 5, 6];
+const a3 =  a1.concat(a2); // utilizando a função concat
+console.log(a3);
+
+const a4 = ai.concat(a2, [7, 8, 9], 'Luiz'); // utilizando o concat
+console.log(a4); 
+
+const a5 = [...a1, ...a2] // utilisando o spread operator
+console.log(a5);
